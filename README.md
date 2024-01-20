5d8929baf246a6b22ab41bddef09afcb53ebebef
Lamiaa Jouwad <JOUWAD_LAMIAA@emsi-edu.ma>
2024-01-20 11:11:59 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+<h1>
+Compte rendu
+</h1>
+
+***
+<h2>Index</h2>
+<li><img src="Captures/index.png"></li>
+
+<h2>Docker</h2>
+<li><img src="Captures/Docker.png"></li>
---
+++ src/test/java/org/sid/springcloudkafka/SpringcloudKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.springcloudkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
